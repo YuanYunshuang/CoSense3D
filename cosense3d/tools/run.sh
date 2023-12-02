a14401a60f94ac0d83dbfc3e028db1529bc96ee3
Yunshuang <yuanyunshuang@163.com>
2023-12-02 18:57:19 +0100

    update

--- cosense3d/tools/run.sh
+++ cosense3d/tools/run.sh
@@ -12,6 +12,6 @@ cd /mars/projects20/CoSense3D
 #PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/voxelnet_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/voxelnet_ego_grad/epoch50.pth
 #PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/fpvrcnn_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/fpvrcnn_all_grad/epoch50.pth
 #PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/fpvrcnn_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/fpvrcnn_ego_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_all_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_ego_grad/epoch50.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_all_grad/epoch50.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_ego_grad/epoch50.pth
 PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d/epoch50.pth
