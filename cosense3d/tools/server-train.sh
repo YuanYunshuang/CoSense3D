64dcd78a8cc57a775aebeb88fad73e3de01681f8
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2024-01-10 10:39:25 +0100

    update

--- cosense3d/tools/server-train.sh
+++ cosense3d/tools/server-train.sh
@@ -8,5 +8,5 @@ cosense3d/tools/agent_runner.py \
 --config ./cosense3d/config/stream_lidar_st_v2.yaml \
 --mode train \
 --gpus 2 \
---data_path /koko/yunshuang/OPV2V/temporal \
---meta_path /koko/yunshuang/cosense3d/opv2v_temporal
+--data-path /koko/yunshuang/OPV2V/temporal \
+--meta-path /koko/yunshuang/cosense3d/opv2v_temporal
