4cda79915566cb3e925c038888d7e6e64d4813d3
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2024-01-10 18:42:19 +0100

    update

--- cosense3d/tools/server-train.sh
+++ cosense3d/tools/server-train.sh
@@ -4,13 +4,13 @@ export OMP_NUM_THREADS=16
 export PYTORCH_CUDA_ALLOC_CONF=MAX_SPLIT_SIZE_MB=256
 
 torchrun \
---nproc_per_node=4 \
+--nproc_per_node=2 \
 cosense3d/tools/agent_runner.py \
 --config ./cosense3d/config/stream_lidar_st_v3.yaml \
 --mode train \
---gpus 4 \
+--gpus 2 \
 --data-path /koko/yunshuang/OPV2V/temporal \
 --meta-path /koko/yunshuang/cosense3d/opv2v_temporal \
 --log-dir /koko/yunshuang/train_out/cosense3d \
 --run-name StreamLTSv3 \
---batch-size 2
+--batch-size 3
