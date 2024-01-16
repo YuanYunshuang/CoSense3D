b2b8d1b93bafb07a73399d9b2c3ec430440b49da
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2024-01-16 13:59:39 +0100

    update

--- cosense3d/agents/core/data_manager.py
+++ cosense3d/agents/core/data_manager.py
@@ -377,6 +377,8 @@ class SeqDataManager:
     def distribute_to_cav(self, data):
         for l, d in enumerate(data):
             valid_agent_ids = d['valid_agent_ids']
+            print(l, valid_agent_ids)
+            print('##################')
             global_data_list = []
             for b, agent_ids in enumerate(valid_agent_ids):
                 global_data = {}
