b167c432492b3a9f3b476787b5779a2b93e40d8a
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2024-01-16 17:38:48 +0100

    update

--- cosense3d/agents/center_controller.py
+++ cosense3d/agents/center_controller.py
@@ -139,29 +139,29 @@ class CenterController:
         # process local cav data
         if 'no_grad' in batched_tasks[0] and len(batched_tasks[0]['no_grad']) > 0:
             self.forward_runner(batched_tasks[0]['no_grad'], with_grad=False, **kwargs)
-        # if kwargs['itr'] == 6:
-        #     print('1')
+        if kwargs['itr'] == 6:
+            print(f"{kwargs['gpu_id']}: 1")
         self.forward_runner(batched_tasks[0]['with_grad'], with_grad=training_mode, **kwargs)
         # if kwargs['itr'] == 6:
         #     print('2')
         # process tasks that needs to be run sequentially
         seq_tasks = self.task_manager.parallel_to_sequential(batched_tasks[1])
         for i in range(self.seq_len):
-            if kwargs['itr'] == 6:
-                print(f"{kwargs['gpu_id']}: 2.1")
+            # if kwargs['itr'] == 6:
+            #     print(f"{kwargs['gpu_id']}: 2.1")
             self.cav_manager.apply_cav_function('pre_update_memory', seq_idx=i)
-            if kwargs['itr'] == 6:
-                print(f"{kwargs['gpu_id']}: 2.2")
+            # if kwargs['itr'] == 6:
+            #     print(f"{kwargs['gpu_id']}: 2.2")
             if 'no_grad' in seq_tasks and len(seq_tasks['no_grad'][i]) > 0:
                 self.forward_runner(seq_tasks['no_grad'][i], with_grad=False, **kwargs)
-            if kwargs['itr'] == 6:
-                print(f"{kwargs['gpu_id']}: 2.3")
+            # if kwargs['itr'] == 6:
+            #     print(f"{kwargs['gpu_id']}: 2.3")
             self.forward_runner(seq_tasks['with_grad'][i], with_grad=training_mode, **kwargs)
-            if kwargs['itr'] == 6:
-                print(f"{kwargs['gpu_id']}: 2.4")
+            # if kwargs['itr'] == 6:
+            #     print(f"{kwargs['gpu_id']}: 2.4")
             self.cav_manager.apply_cav_function('post_update_memory', seq_idx=i)
-            if kwargs['itr'] == 6:
-                print(f"{kwargs['gpu_id']}: 2.5")
+            # if kwargs['itr'] == 6:
+            #     print(f"{kwargs['gpu_id']}: 2.5")
         # if kwargs['itr'] == 6:
         #     print('3')
         # send coop cav feature-level cpm to ego cav
