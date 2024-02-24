9d3f2dedfdfdb71e3616e696638eddeb2ee01d97
Yunshuang <yuanyunshuang@163.com>
2024-02-24 17:54:31 +0100

    streamLTS on dairv2x

--- cosense3d/tools/server-train.sh
+++ cosense3d/tools/server-train.sh
@@ -6,13 +6,11 @@ export PYTORCH_CUDA_ALLOC_CONF=MAX_SPLIT_SIZE_MB=256
 torchrun \
 --nproc_per_node=4 \
 cosense3d/tools/agent_runner.py \
---config ./cosense3d/config/stream_lidar_st_v6_s.yaml \
+--config ./cosense3d/config/stream_lidar_st_v2_dairv2x.yaml \
 --mode train \
 --gpus 4 \
---data-path /koko/yunshuang/OPV2V/temporal \
---meta-path /koko/yunshuang/cosense3d/opv2v_temporal \
 --log-dir /koko/yunshuang/train_out \
---run-name StreamLTS_seq8 \
+--run-name StreamLTS_dairv2x \
 --batch-size 1 \
 --n-workers 8 \
 #--resume-from /koko/yunshuang/train_out/StreamLTS_seq8/epoch1.pth
