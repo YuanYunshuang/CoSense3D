e4f6ac02d804cf6ee577aacdb969b615f62c0614
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2024-02-26 08:12:54 +0100

    StreamLTS_fcooper_dairv2x: server train

--- cosense3d/tools/server-train.sh
+++ cosense3d/tools/server-train.sh
@@ -6,11 +6,11 @@ export PYTORCH_CUDA_ALLOC_CONF=MAX_SPLIT_SIZE_MB=256
 torchrun \
 --nproc_per_node=4 \
 cosense3d/tools/agent_runner.py \
---config ./cosense3d/config/stream_lidar_st_v2_dairv2x.yaml \
+--config ./cosense3d/config/streamLTS_fcooper_dairv2x.yaml \
 --mode train \
 --gpus 4 \
 --log-dir /koko/yunshuang/train_out \
---run-name StreamLTS_dairv2x \
+--run-name StreamLTS_fcooper_dairv2x \
 --batch-size 1 \
 --n-workers 8 \
 #--resume-from /koko/yunshuang/train_out/StreamLTS_fcooper_dairv2x/epoch30.pth
