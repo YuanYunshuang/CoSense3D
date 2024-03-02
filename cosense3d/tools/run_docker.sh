9a2f638f5ea4fe3d22af28eab2165f0978337669
Yunshuang <yuanyunshuang@163.com>
2024-03-02 22:22:16 +0100

    update

--- cosense3d/tools/run_docker.sh
+++ cosense3d/tools/run_docker.sh
@@ -3,6 +3,6 @@
 
 
 
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_opv2vt.yaml --mode train --resume-from /data/CoSense3D/cosense3d/logs/LTS_attnfusion_opv2v_v2/epoch49.pth
 PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_dairv2xt.yaml --mode train --resume-from /data/CoSense3D/cosense3d/logs/LTS_attnfusion_dairv2x_v2/epoch49.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_opv2vt.yaml --mode train --resume-from /data/CoSense3D/cosense3d/logs/LTS_attnfusion_opv2v_v2/epoch49.pth
 PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fpvrcnn_dairv2xt.yaml --mode train --resume-from /data/CoSense3D/cosense3d/logs/LTS_fpvrcnn_dairv2x/epoch20.pth
