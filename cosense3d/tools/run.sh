82b68bf56b61cc04fb841ddbb184eed1e963a748
Yunshuang <yuanyunshuang@163.com>
2024-03-02 21:15:25 +0100

    update

--- cosense3d/tools/run.sh
+++ cosense3d/tools/run.sh
@@ -8,13 +8,4 @@ PYTHONPATH=/mars/projects20/CoSense3D
 #done
 
 cd /mars/projects20/CoSense3D
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/voxelnet_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/voxelnet_all_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/voxelnet_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/voxelnet_ego_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/fpvrcnn_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/fpvrcnn_all_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/fpvrcnn_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/fpvrcnn_ego_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_all_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_ego_grad/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_exp/epoch50.pth
-#PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_exp_no_downsample/epoch50.pth
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/sp3d_cav_v2.yaml --mode test --load-from /media/yuan/luna/cosense3d/sp3d_anchor/epoch50.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_opv2vt.yaml --mode train --resume-from /media/yuan/luna/streamLTS/LTS_dairv2x/epoch40.pth
