9be6af6059068023f9547807c2d9be1c1cba4b82
Yunshuang <yuanyunshuang@163.com>
2024-03-03 10:29:16 +0100

    update

--- cosense3d/agents/core/train_runner.py
+++ cosense3d/agents/core/train_runner.py
@@ -91,13 +91,15 @@ class TrainRunner(BaseRunner):
             if resume_from is not None:
                 self.start_epoch = ckpt['epoch'] + 1
                 self.epoch = ckpt['epoch'] + 1
-                # self.lr_scheduler.load_state_dict(ckpt['lr_scheduler'])
-                # try:
-                #     self.optimizer.load_state_dict(ckpt['optimizer'])
-                # except:
-                #     warnings.warn("Cannot load optimizer state_dict, "
-                #                   "there might be training parameter changes, "
-                #                   "please consider using 'load-from'.")
+                if 'lr_scheduler' in ckpt:
+                    self.lr_scheduler.load_state_dict(ckpt['lr_scheduler'])
+                try:
+                    if 'optimizer' in ckpt:
+                        self.optimizer.load_state_dict(ckpt['optimizer'])
+                except:
+                    warnings.warn("Cannot load optimizer state_dict, "
+                                  "there might be training parameter changes, "
+                                  "please consider using 'load-from'.")
 
     def run(self):
         with torch.autograd.set_detect_anomaly(True):
