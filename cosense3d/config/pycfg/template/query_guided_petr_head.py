a470c32e57649ed266613cf9fa76606be3841834
Yunshuang <yuanyunshuang@163.com>
2024-03-03 14:17:40 +0100

    update

--- cosense3d/config/pycfg/template/query_guided_petr_head.py
+++ cosense3d/config/pycfg/template/query_guided_petr_head.py
@@ -18,7 +18,7 @@ def get_query_guided_petr_head_cfg(gather_keys, scatter_keys, gt_keys,
             cls_assigner=dict(
                 type='target_assigners.BEVHardCenternessAssigner',
                 n_cls=1,
-                min_radius=1.6,
+                min_radius=1.0,
                 pos_neg_ratio=0,
                 mining_thr=0,
             ),
