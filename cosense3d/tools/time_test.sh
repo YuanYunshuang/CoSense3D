d6242c15e60960b348e13a2c64da106cec56a618
Yunshuang <yuanyunshuang@163.com>
2024-03-03 18:55:21 +0100

    update

--- cosense3d/tools/time_test.sh
+++ cosense3d/tools/time_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_opv2vt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_opv2v/epoch49.pth
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fcooper_opv2vt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_fcooper_opv2v/epoch10.pth
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_opv2vt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_attn_opv2v/epoch10.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_opv2vt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_opv2v/epoch44.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fcooper_opv2vt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_fcooper_opv2v/epoch10.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_opv2vt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_attn_opv2v/epoch10.pth
 
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_dairv2xt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_dairv2x/epoch47.pth
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fcooper_dairv2xt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_fcooper_dairv2x/epoch10.pth
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_dairv2xt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_attn_dairv2x/epoch10.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_dairv2xt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_dairv2x/epoch47.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fcooper_dairv2xt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_fcooper_dairv2x/epoch10.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_attnfusion_dairv2xt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_attn_dairv2x/epoch10.pth
 
 
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fpvrcnn_opv2vt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_fpvrcnn_opv2v/epoch10.pth
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fpvrcnn_dairv2xt.yaml --mode train --resume-from /media/yuan/luna/LTS_time_test/LTS_fpvrcnn_dairv2x/epoch10.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fpvrcnn_opv2vt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_fpvrcnn_opv2v/epoch10.pth
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_fpvrcnn_dairv2xt.yaml --mode train --load-from /media/yuan/luna/LTS_time_test/LTS_fpvrcnn_dairv2x/epoch10.pth
