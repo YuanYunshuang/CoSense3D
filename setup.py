5b3f8239e71e90f4794bd7ad96781d15d2f67f25
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2024-03-07 09:54:15 +0100

    update

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ __version__ = '0.0.1'
 setup(
     name='cosense3d',
     version=__version__,
-    author='Yunshuang Yuan',
-    author_email='yunshuang.yuan@ikg.uni-hannover.de',
+    author='xx',
+    author_email='example@gmail.com',
     url='-',
     license='MIT',
     packages=find_packages(include=['cosense3d']),
