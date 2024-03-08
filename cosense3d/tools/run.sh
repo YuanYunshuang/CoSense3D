68e7bdf924ee8e02ea4e63747a30d1835a1ef7f4
Yunshuang <yuanyunshuang@163.com>
2024-03-08 11:49:22 +0100

    update

--- cosense3d/tools/run.sh
+++ cosense3d/tools/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_dairv2xt.yaml --mode train \
---resume-from /media/yuan/luna/streamLTS/LTS_dairv2x/epoch50.pth --batch-size 4
-PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_dairv2xt_roi_focal_loss.yaml --mode train \
---resume-from /media/yuan/luna/streamLTS/LTS_dairv2x/epoch50.pth --batch-size 4
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_dairv2xt.yaml --mode train --run-name LTS-dair --batch-size 4
+PYTHONPATH=. python cosense3d/tools/agent_runner.py --config ./cosense3d/config/streamLTS_dairv2xt_roi_focal_loss.yaml --mode train --run-name LTS-dair-roi_focal --batch-size 4
